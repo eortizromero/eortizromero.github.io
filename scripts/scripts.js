8a1ef560b6ccd9e2f35cd1b6a1ea511610ad770b
Edgardo Ortiz <edgardoficial.yo@gmail.com>
2023-07-08 21:44:42 -0600

    [UPDATE] update github website, version 1.0.2

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+let button_contact = document.getElementById("button_contact");
+
+button_contact.onclick = function(evt) {
+    alert("Sorry! we are building this part. Please send me an email to edgardoficial.yo@gmail.com");
+}
